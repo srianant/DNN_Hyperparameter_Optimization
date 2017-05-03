522ec6d11db80a4a9f2817b6b293844dbd9383be
srianant <srianant@users.noreply.github.com>
2017-05-02 19:07:39 -0700

    Delete script_for_nn_simple.sh

--- Optimization/capstone/NeuralNetwork/nn_dist/script_for_nn_simple.sh
+++
@@ -1,4 +0,0 @@
-rm -rf /tmp/nn_dist \
-& python nn_distributed.py --ps_hosts=localhost:2223 --worker_hosts=localhost:2225 --job_name=ps --task_index=0 \
-& python nn_distributed.py --ps_hosts=localhost:2223 --worker_hosts=localhost:2225 --job_name=worker --task_index=0
- 
