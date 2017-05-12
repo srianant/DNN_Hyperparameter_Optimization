c04a219021c21d168f4d5b9153d927f41ad6f97c
srianant <srianant@yhoo.com>
2017-05-12 11:23:15 -0700

    added more inline comments

--- Optimization/capstone/NeuralNetwork/nn_dist/example/tf_dist.py
+++ Optimization/capstone/NeuralNetwork/nn_dist/example/tf_dist.py
@@ -31,6 +31,12 @@ def train_model(mon_sess, global_step, loss):
     print ("%s Result: %d" % (GNODE,result[0]))
 
 def main(unused_argv):
+
+    # Make sure jobs has names and task_index
+    if FLAGS.job_name is None or FLAGS.job_name == "":
+        raise ValueError("Must specify an explicit `job_name`")
+    if FLAGS.task_index is None or FLAGS.task_index == "":
+        raise ValueError("Must specify an explicit `task_index`")
  
     # Construct the cluster and start the server
     ps_spec = FLAGS.ps_hosts.split(",")
@@ -39,16 +45,23 @@ def main(unused_argv):
     # Get the number of workers.
     num_workers = len(worker_spec)
 
+    # Represents a cluster as a set of "tasks", organized into "jobs"
     cluster = tf.train.ClusterSpec({
         "ps": ps_spec,
         "worker": worker_spec})
 
+    # An in-process TensorFlow server, for use in distributed training (per process node)
     server = tf.train.Server(cluster, job_name=FLAGS.job_name,
                              task_index=FLAGS.task_index) 
 
+    # Configure GPU
+    # If allow_growth == true, the allocator does not pre-allocate the entire specified
+    # GPU memory region, instead starting small and growing as needed.
+    # Assume that you have 12GB of GPU memory and want to allocate ~4GB
     gpu_options = tf.GPUOptions(allow_growth=True,
                                 per_process_gpu_memory_fraction=0.333)
 
+    # Session config. When allow_soft_placement==True, it automatically identifies between cpu/gpu 
     sess_config = tf.ConfigProto(
         allow_soft_placement=True,
         log_device_placement=False,
@@ -57,6 +70,7 @@ def main(unused_argv):
 
     if FLAGS.job_name == "ps":
         print ("%s ps: server join"%GNODE)
+        # ps: will go into listening mode and communicates with worker using gPRC
         server.join()
     elif FLAGS.job_name == "worker":
         is_chief = (FLAGS.task_index == 0)
@@ -64,13 +78,19 @@ def main(unused_argv):
         gpu = (FLAGS.task_index % 1)
         worker_device = "/job:worker/task:%d/gpu:%d" % (FLAGS.task_index, gpu)
 
+        # Builds Graphs for each replicas (processing nodes). 
+        # The device setter will automatically place Variables ops on separate parameter servers (ps). 
+        # The non-Variable ops will be placed on the workers.
+        # The ps use CPU and workers use corresponding GPU
         with tf.device(
                 tf.train.replica_device_setter(
                     worker_device=worker_device,
                     ps_device="/job:ps/cpu:0",
                     cluster=cluster)):
 
+            # User specified hooks. In this case it represents the number of training steps
             hooks = [tf.train.StopAtStepHook(last_step=10)]
+            # Monitoring session require gloabl_step to be defined
             global_step = tf.Variable(0, name="global_step", trainable=False)
 
             # Build TF model 
@@ -78,10 +98,14 @@ def main(unused_argv):
 
         time_begin = time.time()
 
+        # For a chief, this utility sets proper session initializer/restorer. 
+        # It also creates hooks related to checkpoint and summary saving. 
+        # For workers, this utility sets proper session creator which waits for the chief to initialize/restore.
         with tf.train.MonitoredTrainingSession(master=server.target,
                                                is_chief=is_chief,
                                                hooks=hooks,
                                                config=sess_config) as mon_sess:
+            # Train in loop until condition
             while not mon_sess.should_stop():
                 # Train TF model 
                 train_model(mon_sess, global_step, loss)
